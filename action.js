37a4fc3e940b4fde0b1175976118efab6d4edb95
644792799 <644792799@qq.com>
2016-11-14 00:26:14 +0800

    添加缓存

--- action.js
+++ action.js
@@ -1,11 +1,15 @@
 (function(){
   var $=function(id){return document.getElementById(id);}
+  var _$=function(cls){return document.getElementsByClassName(cls);}
   var msg = "";
+  var cookiemsg = "";
   var interval = 10;//unit minute
   var intervalID = "";
   var countDown = 0;
   var intervalCountDownID = "";
+  var chatContentLength = 0;
   var actions = {
+    cookieName:"ZSMessage",
     $ok:$('ok'),
     $message:$('message'),
     $jssendmsg:$('js-send-msg'),
@@ -19,6 +23,8 @@
       ipt.setAttribute("class","DyAutoMessageTxt");
       ipt.setAttribute("id","DyAutoMessageTxt");//
       ipt.setAttribute("placeholder","请输入连续发送的弹幕！！！");
+      cookiemsg = actions.getCookie(actions.cookieName);
+      ipt.value = cookiemsg;
 
       var btn = document.createElement("input");
       btn.setAttribute("type","button");
@@ -75,6 +81,10 @@
         }
         actions.startSend();
       }, true);
+
+      // _$('c-list')[0].onchange = function(){
+      //   console.log(1111);
+      // };
     },
     findSendBtn: function(){
       var nodes = actions.$jssendmsg.childNodes;
@@ -97,6 +107,7 @@
   		}
     },
     setcountdown: function(countDown, lbldom){
+      actions.chatContentListener();
       lbldom = lbldom || $('DyAutoMessageCountDownLbl');
       if(countDown == 0){
         lbldom.innerHTML = "已停止";
@@ -104,8 +115,19 @@
         lbldom.innerHTML = "还剩:" + countDown-- + "秒";
       }
     },
+    chatContentListener: function(){
+       var children = _$('c-list')[0].childNodes;
+       if(children.length != chatContentLength){
+         chatContentLength = children.length;
+         var ih = children[children.length - 1].getElementsByTagName('a')[1].text;
+         console.log(ih);
+       }
+    },
     startSend: function(){
       if(msg.Trim() != ""){
+        if(msg != cookiemsg){
+          actions.setCookie(actions.cookieName, msg, "7d");
+        }
         countDown = interval*60;
         intervalID = window.setInterval(actions.send, interval*60*1000);
         intervalCountDownID = window.setInterval(actions.countdown, 1000);
@@ -116,6 +138,42 @@
     endSend: function(){
       window.clearInterval(intervalID);
       window.clearInterval(intervalCountDownID);
+    },
+    setCookie: function(name,value,time){
+      var strsec = actions.getsec(time);
+      var exp = new Date();
+      exp.setTime(exp.getTime() + strsec*1);
+      document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+    },
+    getCookie: function(name){
+      var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+      if(arr=document.cookie.match(reg))
+      return unescape(arr[2]);
+      else
+      return "";
+    },
+    delCookie: function(name){
+      var exp = new Date();
+      exp.setTime(exp.getTime() - 1);
+      var cval=getCookie(name);
+      if(cval!="")
+      document.cookie= name + "="+cval+";expires="+exp.toGMTString();
+    },
+    getsec:function(str){
+      var str1=str.substring(1,str.length)*1;
+      var str2=str.substring(0,1);
+      if (str2=="s")
+      {
+        return str1*1000;
+      }
+      else if (str2=="h")
+      {
+        return str1*60*60*1000;
+      }
+      else if (str2=="d")
+      {
+        return str1*24*60*60*1000;
+      }
     },
     init: function(){
       actions.addHtml();
